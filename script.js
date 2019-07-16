5e6a2b3a72d0221780b1dd92e654d6abd1efd863
Samreen Shehzad <49517251+samreenshehzad@users.noreply.github.com>
2019-07-16 15:57:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// let student={
+// name:"samreen",
+// age:32,
+// hobbies:["reading","sleeping","cooking"];
+// phone:5667788,
+// email:"sam@SpeechGrammarList.com"
+// }
+// arrays in json
+// let hobbies= ["bookreading","playing","cooking",{ name:"samreen"}]
+let aghaKhanUniversity={
+    InterimCEO:"ShaguftaHassan",
+    ChiefMedicalOfficer:"Dr.AsimBelgaumi",
+    SeniorManager:"ZeeshanFaheem",
+    ChiefNursingOfficer:"ChiefNursingOfficer",
+    Secretary,CEOsOffice:"ZohraFazal",
+    Director,HumanResources:"ShamimNayani",
+    Director,MarCom:"ShamsahVirani"
+}
+
+
+
+
