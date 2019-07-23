9f812174530e7401c307d8ed9f15b7f7b4ff765e
Samreen Shehzad <49517251+samreenshehzad@users.noreply.github.com>
2019-07-23 15:52:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// let student={
-// name:"samreen",
-// age:32,
-// hobbies:["reading","sleeping","cooking"];
-// phone:5667788,
-// email:"sam@SpeechGrammarList.com"
-// }
-// arrays in json
-// let hobbies= ["bookreading","playing","cooking",{ name:"samreen"}]
-let aghaKhanUniversity={
-    InterimCEO:"ShaguftaHassan",
-    ChiefMedicalOfficer:"Dr.AsimBelgaumi",
-    SeniorManager:"ZeeshanFaheem",
-    ChiefNursingOfficer:"ChiefNursingOfficer",
-    Secretary,CEOsOffice:"ZohraFazal",
-    Director,HumanResources:"ShamimNayani",
-    Director,MarCom:"ShamsahVirani"
+let count;
+let randomNumber;
+function resetGame() {
+    randomNumber = parseInt(Math.random() * 101);
+    count = 0;
 }
+resetGame();
+function guessTheNumber() {
+    let userInput = parseInt(document.querySelector('input').value);
 
+    if (userInput > randomNumber) {
+        alert("Too Big");
+        count++;
+    } else if (userInput < randomNumber) {
+        alert("Too Small");
+    }
 
-
-
+} 
