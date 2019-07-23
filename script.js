01420241f9a47494daff83e395d8fa0065757c01
Samreen Shehzad <49517251+samreenshehzad@users.noreply.github.com>
2019-07-23 17:56:28 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-let count;
-let randomNumber;
-function resetGame() {
-    randomNumber = parseInt(Math.random() * 101);
-    count = 0;
-}
-resetGame();
-function guessTheNumber() {
-    let userInput = parseInt(document.querySelector('input').value);
+    let randomNumber;
+    let wrongGuessCount=0;
 
-    if (userInput > randomNumber) {
-        alert("Too Big");
-        count++;
-    } else if (userInput < randomNumber) {
-        alert("Too Small");
+    resetGame
+    function resetGame() {
+    randomNumber = parseInt(Math.random() * 100);
+     wrongGuessCount = 0;
     }
+    resetGame();
+    function guessTheNumber() {
+    let userInput = parseInt(document.querySelector("input").value);
 
-} 
+    if(userInput < randomNumber) {
+        alert("Too Small");
+        wrongGuessCount++;
+    }
+    else if(userInput > randomNumber) {
+        alert("Too Big");
+        wrongGuessCount++;
+    }
+    else if(userInput== randomNumber){
+        alert("Congratulations wrong guessses:" + wrongGuessCount);
+        resetGame();
+    }
+    }
